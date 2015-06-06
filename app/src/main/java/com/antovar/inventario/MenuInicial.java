aab4888fe0f8bb339bee3b8b5b32c55fda28fd3c
Antonio Tovar <antovar@yahoo.com>
2015-06-06 22:49:43 +0200

    Creo el fichero

--- app/src/main/java/com/antovar/inventario/MenuInicial.java
+++ app/src/main/java/com/antovar/inventario/MenuInicial.java
@@ -13,8 +13,8 @@ import java.io.IOException;
 
 public class MenuInicial extends Activity {
 
-    public boolean almacenamiento_disponible;
-    public File path;
+    public boolean almacenamiento_disponible = false;
+    public File fichero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +26,29 @@ public class MenuInicial extends Activity {
     private void comprueba_almacenamiento() {
         if (!isExternalStorageWritable()) {
             Toast.makeText(this, R.string.msg_no_acceso_ext, Toast.LENGTH_LONG).show();
-            almacenamiento_disponible = false;
             return;
         }
         File path = Environment.getExternalStorageDirectory();
         File dir = new File(path, "InventarioCasa");
         Toast.makeText(this, (CharSequence) dir.toString(), Toast.LENGTH_LONG).show();
-        if (!dir.mkdir())
-            if (!dir.isDirectory()) Toast.makeText(this, R.string.msg_no_creado_dir, Toast.LENGTH_LONG).show();
+        if (dir.mkdir()) {
+            Toast.makeText(this, R.string.msg_dir_creado + "InventarioCasa", Toast.LENGTH_LONG).show();
+        } else {
+            if (!dir.isDirectory()) {
+                Toast.makeText(this, R.string.msg_no_creado_dir, Toast.LENGTH_LONG).show();
+            }
+        }
+        fichero = new File(dir, "InventarioCasa.csv");
+        if (!fichero.exists()) {
+            try {
+                if (fichero.createNewFile()) {
+                    Toast.makeText(this, R.string.msg_fich_creado + "InventarioCasa.csv", Toast.LENGTH_LONG).show();
+                }
+            } catch (IOException e) {
+                Toast.makeText(this, R.string.msg_no_creado_fich + "InventarioCasa.csv", Toast.LENGTH_LONG).show();
+            }
+        }
+        almacenamiento_disponible = true;
 //        } catch (Exception e) {
 //            if (!dir.isDirectory()) {
 //                Toast.makeText(this, R.string.msg_no_creado_dir, Toast.LENGTH_LONG).show();
