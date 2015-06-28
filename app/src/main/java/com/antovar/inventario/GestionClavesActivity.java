7003d438cbd26490fd3df6ffe9baeeb21be4a38b
Antonio Tovar <antovar@gmail.com>
2015-06-28 22:59:04 +0200

    inicio viewlist de claves

--- app/src/main/java/com/antovar/inventario/GestionClavesActivity.java
+++ app/src/main/java/com/antovar/inventario/GestionClavesActivity.java
@@ -3,15 +3,40 @@ package com.antovar.inventario;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 import android.view.MenuItem;
 
 
 public class GestionClavesActivity extends Activity {
-
+    protected BDatos bdatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gestion_claves);
+		
+		final ListView listview = (ListView) findViewById(R.id.listViewClaves);
+		final ArrayAdapter adapter = new ArrayAdapter(this,
+			android.R.layout.simple_list_item_1, bdatos.claves);
+		listview.setAdapter(adapter);
+		
+//		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//				@Override
+//				public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+//					final String item = (String) parent.getItemAtPosition(position);
+//					view.animate().setDuration(2000).alpha(0)
+//						.withEndAction(new Runnable() {
+//							@Override
+//							public void run() {
+//								list.remove(item);
+//								adapter.notifyDataSetChanged();
+//								view.setAlpha(1);
+//							}
+//						});
+//				}
+//
+//		});
     }
 
     @Override
